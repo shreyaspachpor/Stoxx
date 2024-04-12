7544b1ff645b0cfcc0ec09e0bea343125747ba1a
Shreyas Pachpor <shreyaspachporr@gmail.com>
2024-04-12 11:10:51 +0530

    images

--- events.js
+++ events.js
@@ -1,3 +1,14 @@
-function displayMessage() {
-    alert("Mutual fund investments are subject to market risks. Please read all scheme related documents carefully before investing. Past performance of the schemes is neither an indicator nor a guarantee of future performance.");
-}
+// Display disclaimer message when the page loads
+window.onload = function() {
+    displayMessage();
+  };
+  
+  // Function to display the disclaimer message
+  function displayMessage() {
+    alert(
+      "Mutual fund investments are subject to market risks. Please read all scheme related documents carefully before investing. Past performance of the schemes is neither an indicator nor a guarantee of future performance."
+    );
+  }
+  
+
+  
